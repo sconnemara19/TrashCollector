22c816f0cbba293513c54da59db4d5280a2f05ab
Simone Connemara <simoneconnemara@outlook.com>
2019-10-28 16:02:35 -0500

    Added Employee

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {   [Key]
+        public int Id { get; set; }
+        public string firstName  { get; set; }
+        public string lastName { get; set; }
+        public string streetAddress { get; set; }
+        public int zipcode { get; set; }
+        public string pickupDay { get; set; }
+        public double balance  { get; set; }
+        public double monthlyCharge { get; set; }
+        public Boolean pickupConfirmed { get; set; }
+        public string  Start { get; set; }
+        public string End  { get; set; }
+
+
+
+
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {   [Key]
+        public int employeeId { get; set; }
+        public int employeeZipcode { get; set; }
+
+
+    }
+
+
+
+}
